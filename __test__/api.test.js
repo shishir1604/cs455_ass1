2df6da6e4e6b6ff525566904df62029a547ad252
ayushyadav07 <105917298+ayushyadav07@users.noreply.github.com>
2024-10-10 18:38:06 +0530

    Create api.test.js to test integration of the server
    add test to check if root is accesed and check success strings and status for request. Similarly checking for post function

---
+++ __test__/api.test.js
@@ -0,0 +1,43 @@
+const req=require('supertest');
+const app=require('../server.cjs');
+const { afterAll } = require('jest-circus');
+const sqlite3=require('sqlite3').verbose();
+let db
+let server
+beforeAll((done) => {
+    server = app.listen(4000, () => {
+        console.log('Server is running on port 4000'); 
+        db = new sqlite3.Database(':memory:', (err) => {
+            if (err) {
+                console.log('error occurred while creating the database', err.message);
+            } else {
+                db.run('CREATE TABLE IF NOT EXISTS scores (id INTEGER PRIMARY KEY AUTOINCREMENT, player TEXT, score INTEGER, date TEXT)', (err) => {
+                    if (err) {
+                        console.log('error occurred while creating the table', err.message);
+                    }
+                    done(); 
+                });
+            }
+        });
+    });
+});
+
+afterAll((done) => {
+    db.close((err) => {
+        if (err) {
+            console.log('error occurred while closing the database', err.message);
+        }
+        server.close();
+        done(); // Close the server
+    });
+});
+describe('GET /',()=>{
+    it('responds with 200',async()=>{
+        await req(app).get('/').expect(200);
+    });
+});
+describe('GET /high-score',()=>{
+    it('responds with 200',async()=>{
+        await req(app).get('/high-score').expect(200);
+    });
+});
