70427c9827443ecf0b7002cf68278bfb2d1bfe48
shishir <98305285+shishir1604@users.noreply.github.com>
2024-10-10 18:31:04 +0530

    Created an express based backend server
    This server can parse the request-response and creates sqlite3 database and using fetch API publishes data on the database

---
+++ server.cjs
@@ -0,0 +1,42 @@
+const express=require('express');
+const cors=require('cors');
+const sqlite3=require('sqlite3').verbose();
+const bodyParser=require('body-parser');
+const path=require('path');
+const app=express();
+const port=3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+const db=new sqlite3.Database(':memory:', (err)=>{
+    if(err){
+        console.log('error ocurred while creating the database',err.message);
+    }
+    else{
+        db.run('CREATE TABLE IF NOT EXISTS scores (id INTEGER PRIMARY KEY AUTOINCREMENT, player TEXT, score INTEGER, date TEXT)', (err)=>{
+            if(err){
+                console.log('error ocurred while creating the table',err.message);
+            }
+        });
+    }
+});
+app.post('/addScore', (req,res)=>{
+    const player=req.body.player;
+    const score=req.body.score;
+    const date=new Date().toISOString();
+    
+    db.run('INSERT INTO scores (player,score,date) VALUES (?,?,?)',[player,score,date],(err)=>{
+        if(err){
+            console.log('error occured while adding',err.message);
+            res.status(500).send('error saving score');
+        }
+        else{
+            res.status(200).send('score saved');
+            console.log('score saved'); 
+        }
+    });
+});
+module.exports=app;
