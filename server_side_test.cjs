404084c44fe96c015452b9e728dc8f07588d1f51
ayushyadav07 <105917298+ayushyadav07@users.noreply.github.com>
2024-11-16 22:06:33 +0530

    added code for test all the server with appropriate load and duration since free host limits

--- server_side_test.cjs
+++ server_side_test.cjs
@@ -3,18 +3,28 @@ import { check, sleep } from 'k6';
 
 export let options = {
     stages: [
-        { duration: '30s', target: 100 },     // Ramp-up to 100 users
-        { duration: '1m', target: 1000 },     // Ramp-up to 1000 users
-        { duration: '1m', target: 10000 },    // Ramp-up to 10,000 users
-        { duration: '30s', target: 0 },       // Ramp-down
-    ],
-};
+        { duration: '1s', target: 5 },     
+        { duration: '1s', target: 10 },    
 
+    ],
+    thresholds: {
+        'http_req_duration': ['p(95)<2000'], 
+        'http_req_failed': ['rate<0.01'],    
+      },
+};
+const gameUrls = [
+    "https://cs455-ass1.onrender.com", 
+    "https://cs455-ass1-1.onrender.com", 
+    "https://twocs455-ass1.onrender.com"
+  ];
+  const leaderboardUrl = "/high-score";
 export default function () {
-    let resGamePage = http.get('http://localhost:3000/');
+    for (let gameUrl of gameUrls){
+    let resGamePage = http.get(gameUrl);
     check(resGamePage, { 'status is 200': (r) => r.status === 200 });
 
-    let resLeaderboard = http.get('http://localhost:3000/high-score');
+    let resLeaderboard = http.get(gameUrl + leaderboardUrl);
     check(resLeaderboard, { 'status is 200': (r) => r.status === 200 });
     sleep(1);
+    }
 }
